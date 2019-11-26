86a9fbcbc8469e3418a092c05768244e6e156881
cole <479096292@qq.com>
2019-11-26 07:37:17 -0800

    add mq function

--- mq/define.go
+++ mq/define.go
@@ -1,1 +1,12 @@
 package mq
+
+import "github.com/c479096292/spinach-disk/common"
+
+// TransferData : 将要写到rabbitmq的数据的结构体
+type TransferData struct {
+	FileHash      string
+	CurLocation   string
+	DestLocation  string
+	DestStoreType common.StoreType
+}
+
