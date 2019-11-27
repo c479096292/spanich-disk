80ec3e973cabf0895ac57ccb60e67570aaa532ca
cole <479096292@qq.com>
2019-11-27 07:45:10 -0800

    update utils

--- utils/helps.go
+++ utils/helps.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"bytes"
 	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
 	"io"
 	"os"
+	"os/exec"
 )
 
 func Sha1(data []byte) string {
@@ -31,4 +33,23 @@ func EncodeMD5(val string) string {
 	m :=md5.New()
 	m.Write([]byte(val))
 	return hex.EncodeToString(m.Sum(nil)) // 将加密后的byte转为string
+}
+
+
+// 执行 linux shell command
+func ExecLinuxShell(s string) (string, error) {
+	//函数返回一个io.Writer类型的*Cmd
+	cmd := exec.Command("/bin/bash", "-c", s)
+
+	//通过bytes.Buffer将byte类型转化为string类型
+	var result bytes.Buffer
+	cmd.Stdout = &result
+
+	//Run执行cmd包含的命令，并阻塞直至完成
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return result.String(), err
 }
