5e9ec4972e6e1afa12940da348567fd2c0a7e26a
cole <479096292@qq.com>
2019-12-01 00:19:11 -0800

    update common part

--- common/store.go
+++ common/store.go
@@ -1,1 +1,14 @@
 package common
+
+type StoreType int
+
+const (
+	_ StoreType = iota
+	StoreLocal
+	StoreOss
+	StoreCeph
+	// StoreMix : 混合(Ceph及OSS)
+	StoreMix
+	// StoreAll : 所有类型的存储都存一份数据
+	StoreAll
+)
